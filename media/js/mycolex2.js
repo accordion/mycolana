c227d8e872a523a0a7efad18ffa7101a5c679fbf
Stefan Florian Röthlisberger <sfroeth@gmail.com>
2011-08-25 16:44:52 +0200

    Reloads page after a new measurement was added

--- media/js/mycolex2.js
+++ media/js/mycolex2.js
@@ -4,13 +4,23 @@ $(document).ready(function() {
         $(this).datepicker();
     });
     
+    /*
+     * Releads the page after 3 seconds
+     */
+    function reloadPage() {
+        setTimeout('location.reload()', 3000);
+    }
+    
     $(".addMeasurement").click(function() {
         var sidebar = $("#sidebar");
         var obid = $(this).nextAll('input:hidden').val();
         
         sidebar.load('/detail/measure', function() {
             var form = $('#form_measure');
-            form.ajaxForm({target: sidebar});
+            form.ajaxForm({
+                target: sidebar, 
+                success: reloadPage
+            });
             form.find("input:hidden").attr('value', obid);
         });
     });
