8f326e037a0737673ad92676d83b49a3b8e5814a
Stefan Florian Röthlisberger <sfroeth@gmail.com>
2011-08-25 14:24:23 +0200

    Improved layout with CSS

--- classes/view/layout/main.php
+++ classes/view/layout/main.php
@@ -9,12 +9,14 @@ class View_Layout_Main extends Kostache {
     public $header;
     public $content;
     public $footer;
+    public $title = 'Hauptmenü';
     
     public function scripts()
     {
-        $scripts = HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js');
-        $scripts .= HTML::script('https://raw.github.com/malsup/form/master/jquery.form.js');
-        $scripts .= HTML::script('scripts/mycolex2.js');
+        $scripts = HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js') . "\n";
+        $scripts .= HTML::script('https://raw.github.com/malsup/form/master/jquery.form.js') . "\n";
+        $scripts .= HTML::script('media/js/mycolex2.js') . "\n";
         return $scripts;
     }
+
 }
