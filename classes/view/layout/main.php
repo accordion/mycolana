93f3eee0ff0208a26ef7114c4704d81e2e3b649a
Stefan Florian Röthlisberger <sfroeth@gmail.com>
2011-09-05 17:47:12 +0200

    moved the stylesheets and the scripts

--- classes/view/layout/main.php
+++ classes/view/layout/main.php
@@ -8,7 +8,39 @@
 class View_Layout_Main extends Kostache {
     public $header;
     public $content;
-    public $footer;
-    
+    public $footer;    
     public $title;
+       
+    public function scripts()
+    {
+        return array(
+            array('script' => HTML::script('media/js/jquery-1.6.2.min.js')),
+            array('script' => HTML::script('media/js/jquery-ui-1.8.16.custom.min.js')),
+            array('script' => HTML::script('media/js/jquery.form.js')),
+            array('script' => HTML::script('media/js/mycolex2.js')),
+        );
+    }
+    
+    public function styles()
+    {
+        $screen = array('media' => 'screen, projection');
+        $print = array('media' => 'print');
+        
+        return array(
+            // <!-- Blueprint Stylesheets -->
+            array('style' => HTML::style('media/css/blueprint/screen.css', $screen)),
+            array('style' => HTML::style('media/css/blueprint/print.css', $print)),
+            array('style' => '<!--[if IE]-- >'),
+            array('style' => HTML::style('media/css/blueprint/ie.css', $screen)),
+            array('style' => '<![endif]-->'),
+            // <!-- Fancy Type Plugin -->
+            array('style' => HTML::style('media/css/blueprint/plugins/fancy-type/screen.css', $screen)),
+            // <!-- Button plugin -->
+            array('style' => HTML::style('media/css/blueprint/plugins/buttons/screen.css', $screen)),
+            // <!-- custom style -->
+            array('style' => HTML::style('media/css/style.css', $screen)),
+            // <!-- jquery-ui smoothness style -->
+            array('style' => HTML::style('media/css/jquery-ui.css', $screen))
+        );
+    }
 }
