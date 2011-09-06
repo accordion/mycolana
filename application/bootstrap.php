06e7e1d50e631310490e4068b0f000170b8f5c1d
Stefan Florian Röthlisberger <sfroeth@gmail.com>
2011-09-06 16:31:15 +0200

    Changed the base url to /mycolana

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -80,7 +80,7 @@ if (isset($_SERVER['KOHANA_ENV']))
  * - boolean  caching     enable or disable internal caching                 FALSE
  */
 Kohana::init(array(
-    'base_url'   => 'http://localhost/',
+    'base_url'   => 'http://localhost/mycolana',
     'index_file' => false,
 ));
 
