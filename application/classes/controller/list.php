92531cb77f642bc067b50983df524e65932f8bd4
Stefan Florian Röthlisberger <sfroeth@gmail.com>
2011-09-12 12:00:36 +0200

    Use custom view implementation if it exists for the list

--- application/classes/controller/list.php
+++ application/classes/controller/list.php
@@ -24,35 +24,31 @@ class Controller_List extends Controller_Base {
                        $elements->and_where($column, '=', $value);
                 }
             }
+            // if query param object=n is set, get only the rows which belong to that object
             else if(($object_id = $this->request->query('object')) != null)
             {
                 $object = Model_Base::factory('object', $object_id);
                 $model = $model_name . 's';
                 $elements = $object->$model;
             }
-            $this->set_content_view(new View_List_Generic($elements));
+            
+            // Set the custom view if a custom implementation exists
+            $view = null;
+            $class = 'View_List_' . ucfirst($model_name);
+            if(class_exists($class)) {
+                $view = new $class($elements);
+            }
+            else 
+            {
+                $view = new View_List_Generic($elements);  
+            }
+            
+            $this->set_content_view($view);
         }
         else // Index action
         {
             $this->set_content_view(new View_List_Index); 
         }
     }
-    
-//    public function action_object()
-//    {
-//        $objects = Model_Base::factory('object');
-//
-//        if($this->request->query('search') !== null)
-//        {
-//            $query = SessionHandler::get_search_query('object');
-//            foreach(array_keys($objects->list_columns()) as $column)
-//            {
-//                $value = Arr::get($query, $column, '');
-//                if($value != '')
-//                   $objects->and_where($column, '=', $value);
-//            }
-//        }
-//        $this->set_content_view(new View_List_Object($objects));
-//    }
     
 }
