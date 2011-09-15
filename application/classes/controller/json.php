550b294d15a2aac996d3140121b2764f39834c9d
Stefan Florian Röthlisberger <sfroeth@gmail.com>
2011-09-15 13:05:05 +0200

    Implemented assignation function

--- application/classes/controller/json.php
+++ application/classes/controller/json.php
@@ -13,13 +13,52 @@ class Controller_JSON extends Controller {
         
         $model_name = $this->request->param('model');
         $id = $this->request->param('id');
+        $model = null;
+        
+        switch($this->request->method())
+        {
+            case HTTP_Request::GET:
+                $model = Model_Base::factory($model_name, $id);            
+                break;
+            case HTTP_Request::POST:
+                if($this->is_search())
+                {
+                    $model = $this->handle_search($model_name);
+                }
+                break;
+            default:
+                throw new HTTP_Exception_400;
+        }      
+        
+        $this->return_json($model);
+    }
+    
+    private function is_search() 
+    {
+        return isset($_POST['search']);
+    }
+    
+    private function handle_search($model_name)
+    {
+        Kohana_Log::instance()->add(Kohana_Log::WARNING, 'is search');
+        
+        $model = Model_Base::factory($model_name);
+        foreach(array_keys($model->list_columns()) as $column)
+        {
+            $value = Arr::get($_POST, $column, '');
+            if($value != '')
+               $model->and_where($column, '=', $value);
+        }
+        return $model;
+    }
+    
+    private function return_json($model) {
         $heading = null;
         $data = array();
         
         // generate heading
         if($this->request->query('withHeading') !== null)
-        {
-            $model = Model_Base::factory($model_name);
+        {           
             $heading = array();
             foreach(array_keys($model->as_array()) as $column)
             {
@@ -27,15 +66,14 @@ class Controller_JSON extends Controller {
             }
         }
         
-        if($id !== null)
+        if($model->loaded())
         {
-            $model = Model_Base::factory($model_name, $id);
             $data[] = $model->as_array();
         }
         else 
         {
             $models = array();
-            foreach(Model_Base::factory($model_name)->find_all() as $model)
+            foreach($model->find_all() as $model)
             {
                 $models[] = $model->as_array();
             }
@@ -46,5 +84,5 @@ class Controller_JSON extends Controller {
         $json['data'] = $data;
         echo json_encode($json);
     }
-    
+     
 }
